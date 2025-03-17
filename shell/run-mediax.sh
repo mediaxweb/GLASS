b8f57cb4533990f222f0650535d259094ca13559
Blockchain Coder <ndtuan89@gmail.com>
2025-03-17 10:40:47 +0700

    Update run-mediax.sh augpath

--- shell/run-mediax.sh
+++ shell/run-mediax.sh
@@ -1,5 +1,5 @@
 datapath=/root/cqy/dataset/MVTec
-augpath=/root/cqy/dataset/dtd/images
+augpath=/content/dtd
 classes=('carpet' 'grid' 'leather' 'tile' 'wood' 'bottle' 'cable' 'capsule' 'hazelnut' 'metal_nut' 'pill' 'screw' 'toothbrush' 'transistor' 'zipper')
 flags=($(for class in "${classes[@]}"; do echo '-d '"${class}"; done))
 
