0b2752842666955d684295a521459e15c187de6b
Blockchain Coder <ndtuan89@gmail.com>
2025-03-20 17:10:45 +0700

    Update run-mediax.sh

--- shell/run-mediax.sh
+++ shell/run-mediax.sh
@@ -33,5 +33,6 @@ python /content/GLASS/main.py \
     --fg 1 \
     --rand_aug 1 \
     --batch_size 8 \
+    --imagesize 900 "${flags[@]}" mvtec $datapath $augpath
     #--resize 288 \
-    --imagesize 288 "${flags[@]}" mvtec $datapath $augpath
+    
